6991d531d0c91541f2bb6e11b3c0f408f0c799c4
Tinica Adrian <118120924+adriantinica@users.noreply.github.com>
2023-06-07 10:50:01 +0300

    Add files via upload

---
+++ main/CabinetApp.java
@@ -0,0 +1,44 @@
+
+import skeleton.Thing;
+import skeleton.Cabinet;
+import things.Book;
+import things.Phone;
+import things.Tablet;
+
+public class CabinetApp {
+    public static void main(String[] args) {
+        
+        Thing Phone1 = new Phone("Nokia 6300");
+        Thing Phone2 = new Phone("Nokia 6303 classic");
+
+        Thing Book1 = new Book("Learn Java in 1 Day");
+        Thing Tablet1 = new Tablet("Samsung Galaxy Tab A7 Lite");
+        //Thing [] ColectiaDeAur ={
+        //    new Book("Learn Java in 1 Day"),
+        //    new Book("Learn Java in 1 Day"),
+        //    new Book("Learn Java in 1 Day"),
+        
+        //};
+
+        Cabinet Biblioteca = new Cabinet();
+
+        Biblioteca.put("middleShelf", Phone1);
+        Biblioteca.put("middleShelf", Phone2);
+        Biblioteca.put("topShelf", Book1);
+        Biblioteca.put("bottomShelf", Tablet1 );
+        //Biblioteca.put("middleShelf", ColectiaDeAur);
+        System.out.println(Biblioteca);
+        System.out.println("\n");
+
+        Biblioteca.take("middleShelf" );
+        System.out.println(Biblioteca);
+        System.out.println("\n");
+
+        
+
+
+
+    }
+
+    
+}
